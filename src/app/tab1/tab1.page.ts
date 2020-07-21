26e7dd3f505e4305568566e4e315a6ee1ab7dcae
Brent Williams <brentw@lesterelectrical.com>
2020-07-21 10:17:05 -0500

    test for cordova-android 9.0.0

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var ble;
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -9,4 +11,17 @@ export class Tab1Page {
 
   constructor() {}
 
+  scan() {
+    ble.scan([], 5, function(device) {
+      console.log(JSON.stringify(device));
+    }, function(status) {
+      console.log('Scan failed ' +status);
+    });
+
+    setTimeout(ble.stopScan,
+      5000,
+      function() { console.log("Scan complete"); },
+      function() { console.log("stopScan failed"); }
+    );
+  }
 }
